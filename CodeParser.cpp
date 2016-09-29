e544cecbb7168866c875be342b0076fd97c73eb4
Ilya Kovalenko <ilyacoding@yandex.com>
2016-09-30 00:32:45 +0300

    Добавлено: удаление любых комментариев, разбивание кода на блоки

--- CodeParser.cpp
+++ CodeParser.cpp
@@ -32,6 +32,42 @@ string AddSpaces(string str, int count)
 	return str;
 }
 
+void DeleteComments()
+{
+	bool Delete = false;
+	for (int i = 0; i < s.size() - 1; i++)
+	{
+		for (int j = 0; j < s[i].length(); j++) 
+		{
+			if (Delete) {
+				if (s[i][j] == '*' && s[i][j + 1] == ')') {
+					Delete = false;
+					s[i][j++] = ' ';
+					s[i][j++] = ' ';
+				}
+				else {
+					s[i][j] = ' ';
+				}
+			}
+			if (s[i][j] == '(' && s[i][j + 1] == '*') {
+				Delete = true;
+				j--;
+			}
+		}
+	}
+
+	for (int i = 0; i < s.size() - 1; i++)
+	{
+		for (int j = 0; j < s[i].length(); j++)
+		{
+			if (s[i][j] == '/' && s[i][j + 1] == '/')
+				s[i].erase(s[i].find_first_of("/", j), s[i].length());
+			if ((int)s[i][j] < 0)
+				s[i][j] = ' ';
+		}
+	}
+}
+
 void EraseEmptyStrings()
 {
 	for (int i = 0; i < s.size() - 1; i++)
@@ -49,7 +85,7 @@ void EraseEmptyStrings()
 void Process(int CurrLine)
 {
 	int CurrSpaces = (CountLeftSpaces(s[CurrLine]));
-	s.insert(s.begin() + CurrLine, AddSpaces("{", CurrSpaces));
+	s.insert(s.begin() + CurrLine, AddSpaces(";BEGIN;", CurrSpaces));
 	OperatorsUsed++;
 	cout << CurrLine << endl;
 	while ((CountLeftSpaces(s[++CurrLine]) == CurrSpaces) && (CurrLine < s.size() - 1));
@@ -58,7 +94,7 @@ void Process(int CurrLine)
 	if (CurrSpaces > CountLeftSpaces(s[CurrLine]))
 	{
 		cout << CurrSpaces << endl << CountLeftSpaces(s[CurrLine]) << endl;
-		s.insert(s.begin() + CurrLine, AddSpaces("}", CurrSpaces));
+		s.insert(s.begin() + CurrLine, AddSpaces(";END;", CurrSpaces));
 		OperatorsUsed++;
 	}
 	else if (CurrSpaces < CountLeftSpaces(s[CurrLine])) {
@@ -73,7 +109,7 @@ void Process(int CurrLine)
 			}
 			
 
-		s.insert(s.begin() + CurrLine, AddSpaces("}", CurrSpaces));
+		s.insert(s.begin() + CurrLine, AddSpaces(";END;", CurrSpaces));
 		OperatorsUsed++;
 
 		for (int i = 0; i < queue.size() - 1; i++)
@@ -86,6 +122,41 @@ void Process(int CurrLine)
 	}
 }
 
+void AddBlocks(int CurrLine)
+{
+	int CurrSpaces = (CountLeftSpaces(s[CurrLine]));
+	int PredSpaces = 0;
+	vector <int> q;
+
+	boost::algorithm::trim(s[CurrLine]);
+	s[CurrLine] = AddSpaces("{", CurrSpaces) + s[CurrLine];
+
+
+	for (int i = CurrLine + 1; i < s.size(); i++) {
+		if ((CountLeftSpaces(s[i]) < CurrSpaces) || (i == s.size() - 1)) {
+			s[i - 1] = s[i - 1] + "}";
+			break;
+		}
+		else if ((CountLeftSpaces(s[i]) > CurrSpaces) && (PredSpaces == CurrSpaces)) {
+			q.push_back(i);
+		}
+		PredSpaces = CountLeftSpaces(s[i]);
+	}
+	cout << endl << "===" << endl;
+	for (int i = 0; i < q.size(); i++) {
+		cout << "Q: " << q[i] << " |";
+	}
+	if (q.size() == 0)
+		cout << "EMPTY Q";
+	cout << endl << "===" << endl;
+
+	for (int i = 0; i < q.size(); i++) {
+		AddBlocks(q[i]);
+	}
+
+
+}
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
@@ -93,24 +164,66 @@ int main()
 	char buf[500];
 	string str = "";
 	file.open("Program.fs", ios_base::in);
+	file.imbue(std::locale(""));
 	while (!file.eof()) {
 		file.getline(buf, 500);
 		s.push_back(buf);
 	}
 
+	DeleteComments();
 	EraseEmptyStrings();
-	Process(0);
+	AddBlocks(0);
 
+	cout << endl << endl << "====================" << endl;
 	for (int i = 0; i < s.size(); i++)
 	{
 		cout << s[i] << endl;
 	}
+	cout << "====================" << endl;
 
 	file.close();
 
 	system("pause");
-    return 0;
+	return 0;
 }
+
+	/*
+	cout << CurrLine << endl;
+	while ((CountLeftSpaces(s[++CurrLine]) == CurrSpaces) && (CurrLine < s.size() - 1));
+	cout << CurrSpaces << endl << CountLeftSpaces(s[CurrLine]) << endl;
+
+	if (CurrSpaces > CountLeftSpaces(s[CurrLine]))
+	{
+		cout << CurrSpaces << endl << CountLeftSpaces(s[CurrLine]) << endl;
+		s.insert(s.begin() + CurrLine, AddSpaces(";END;", CurrSpaces));
+		OperatorsUsed++;
+	}
+	else if (CurrSpaces < CountLeftSpaces(s[CurrLine])) {
+		//Process(CurrLine);
+		vector <int> queue;
+		int DefSpaces = 1;
+		while ((CountLeftSpaces(s[++CurrLine]) >= CurrSpaces) && (CurrLine < s.size() - 1))
+			if ((CountLeftSpaces(s[CurrLine]) > CurrSpaces) && (DefSpaces))
+			{
+				DefSpaces = CountLeftSpaces(s[CurrLine]);
+				queue.push_back(CurrLine + OperatorsUsed);
+			}
+
+
+		s.insert(s.begin() + CurrLine, AddSpaces(";END;", CurrSpaces));
+		OperatorsUsed++;
+
+		for (int i = 0; i < queue.size() - 1; i++)
+		{
+			cout << "QUEUE: " << queue[i] << endl;
+			//if (queue[i] < s.size() - 1)
+			//Process(queue[i]);
+		}
+
+	}*/
+
+	//Process(0);
+
 	//Process(14);
 	/*
 	enum Token {
--- Program.fs
+++ Program.fs
@@ -1,9 +1,9 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    let x = 5
+    let x = 5 
     if x < 3 then
         printfn "ddd"
         printfn "ddda"
@@ -11,12 +11,16 @@ let main argv =
         printfn "good" 
         if x < 9 then 
             printfn "goooood"
-	if x > 3 then 
+    if x > 3 then 
         printfn "good" 
         if x < 9 then 
             printfn "goooood"
+            if x > 8 then
+             printfn "ggg"
+            if x < 9 then
+              printfn "asdasd"
     printfn "bad"
 	
 	
     System.Console.ReadKey(true) |> ignore 
-    0 // return an integer exit code
+    0 // return an integer exit code*)
